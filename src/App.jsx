9d85b3f424ce690123ce4e69484c5729a385029c
Chris <Djchris114211@gmail.com>
2024-10-17 13:20:21 -0400

    it works I need to format it

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,62 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
-function City(props) {
-  return <div>This is the City component</div>;
+// API: https://ctp-zip-code-api.onrender.com/
+
+function City({ cityData }) {
+  return (
+    <div>
+      <div>
+        <p>{cityData.LocationText}</p>
+      </div>
+      <div>
+        <ul>
+          <li>State: {cityData.State}</li>
+          <li>
+            Location: ({cityData.Lat}, {cityData.Long})
+          </li>
+          <li>Population (estimated): {cityData.EstimatedPopulation}</li>
+          <li>Total Wages: {cityData.TotalWages}</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-function ZipSearchField(props) {
-  return <div>This is the ZipSearchField component</div>;
+function ZipSearchField() {
+  const [data, setData] = useState([]);
+  const [zipCode, setZipCode] = useState("");
+
+  const handleInputChange = (event) => {
+    const newZipCode = event.target.value;
+    setZipCode(newZipCode);
+  };
+
+  useEffect(() => {
+    fetch(`https://ctp-zip-code-api.onrender.com/zip/${zipCode}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        console.log(data);
+      })
+      .catch((error) => console.error("Error:", error));
+  }, [zipCode]);
+
+  return (
+    <div>
+      <form>
+        <input
+          type="text"
+          value={zipCode}
+          onChange={handleInputChange}
+          placeholder="Enter zip code"
+        />
+      </form>
+      {data.map((cityInfo, index) => (
+        <City key={index} cityData={cityInfo} />
+      ))}
+    </div>
+  );
 }
 
 function App() {
@@ -16,11 +66,8 @@ function App() {
         <h1>Zip Code Search</h1>
       </div>
       <div className="mx-auto" style={{ maxWidth: 400 }}>
+        <p>Zip Code: </p>
         <ZipSearchField />
-        <div>
-          <City />
-          <City />
-        </div>
       </div>
     </div>
   );
